65ef326e05cd7d715152bb4bef242c7428d43007
Abhilash Saj <abhilashsaj@gmail.com>
2020-04-26 15:59:56 +0530

    mahout recommender

--- Apache Mahout/Recommender.java
+++ Apache Mahout/Recommender.java
@@ -8,3 +8,22 @@ import org.apache.mahout.cf.taste.neighborhood.UserNeighborhood;
 import org.apache.mahout.cf.taste.recommender.RecommendedItem;
 import org.apache.mahout.cf.taste.recommender.UserBasedRecommender; 
 import org.apache.mahout.cf.taste.similarity.UserSimilarity; 
+
+
+public class Recommender { 
+     public static void main(String args[]){ 
+          try{ 
+	//Creating data model 
+	DataModel datamodel = new FileDataModel(new File("/path/to/dataset.csv ")); 
+	//Creating UserSimilarity object. 
+	UserSimilarity usersimilarity = new PearsonCorrelationSimilarity(datamodel);
+ 	//Creating UserNeighbourHHood object. 
+	UserNeighborhood userneighborhood = new ThresholdUserNeighborhood(3.0, 	usersimilarity, datamodel); 
+	//Create UserRecomender 
+	UserBasedRecommender recommender = new 	GenericUserBasedRecommender(datamodel, userneighborhood, usersimilarity); 	List<RecommendedItem> recommendations = recommender.recommend(2, 3); 
+	for (RecommendedItem recommendation : recommendations) { 
+	       System.out.println(recommendation); 
+	}
+         }catch(Exception e) { } 
+     } 
+} 
