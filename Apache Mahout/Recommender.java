4fb6565f35b0abcccb54b048cdeb31961de12e8a
abhilashsaj <abhilashsaj@gmail.com>
2020-04-26 22:31:51 +0530

    bug fixes

--- Apache Mahout/Recommender.java
+++ Apache Mahout/Recommender.java
@@ -13,21 +13,34 @@ import org.apache.mahout.cf.taste.similarity.UserSimilarity;
 
 public class Recommender { 
      public static void main(String args[]){ 
+	System.out.println("Started execution");
           try{ 
 	//Creating data model 
-	DataModel datamodel = new FileDataModel(new File("Sample_Data.csv")); 
+	DataModel datamodel = new FileDataModel(new File("data.csv")); 
+	System.out.println("\nCreated data model");
+System.out.println(datamodel);
 	//Creating UserSimilarity object. 
 	UserSimilarity usersimilarity = new PearsonCorrelationSimilarity(datamodel);
+System.out.println("\nCreated user similarity");
+System.out.println(usersimilarity);
  	//Creating UserNeighbourHHood object. 
-	UserNeighborhood userneighborhood = new ThresholdUserNeighborhood(3.0, 	usersimilarity, datamodel); 
+	UserNeighborhood userneighborhood = new ThresholdUserNeighborhood(0.1, 	usersimilarity, datamodel); 
+System.out.println("\nCreated user neighbourhood");
+System.out.println(userneighborhood);
 	//Create UserRecomender 
-	UserBasedRecommender recommender = new 	GenericUserBasedRecommender(datamodel, userneighborhood, usersimilarity); 	
+	UserBasedRecommender recommender = new 	GenericUserBasedRecommender(datamodel, userneighborhood, usersimilarity); 
+System.out.println("\n\nRecommender");
+System.out.println(recommender);	
 	List<RecommendedItem> recommendations = recommender.recommend(2, 3); 
+System.out.println("\n\nRecommendations");
+System.out.println(recommendations);
+	System.out.println("\n\nCreated recommendations");
 	for (RecommendedItem recommendation : recommendations) { 
 	       System.out.println(recommendation); 
 	}
          }catch(Exception e) { 
 	  System.out.println(e); 
 	  } 
+	System.out.println("\n\ncompleted execution"); 
      } 
 } 
