8f7a5102a142e00137c8793c26c7da5e9050c75c
abhilashsaj <abhilashsaj@gmail.com>
2020-04-26 22:52:59 +0530

    carl pearson

--- Apache Mahout/Recommender.java
+++ Apache Mahout/Recommender.java
@@ -12,35 +12,33 @@ import org.apache.mahout.cf.taste.similarity.UserSimilarity;
 
 
 public class Recommender { 
+	
      public static void main(String args[]){ 
 	System.out.println("Started execution");
           try{ 
 	//Creating data model 
-	DataModel datamodel = new FileDataModel(new File("data.csv")); 
+	DataModel datamodel = new FileDataModel(new File("Sample_Data.csv")); 
 	System.out.println("\nCreated data model");
-System.out.println(datamodel);
+	System.out.println(datamodel);
+
+
 	//Creating UserSimilarity object. 
 	UserSimilarity usersimilarity = new PearsonCorrelationSimilarity(datamodel);
-System.out.println("\nCreated user similarity");
-System.out.println(usersimilarity);
+	System.out.println("\nCreated user similarity for Pearson Correlation");
  	//Creating UserNeighbourHHood object. 
 	UserNeighborhood userneighborhood = new ThresholdUserNeighborhood(0.1, 	usersimilarity, datamodel); 
-System.out.println("\nCreated user neighbourhood");
-System.out.println(userneighborhood);
 	//Create UserRecomender 
-	UserBasedRecommender recommender = new 	GenericUserBasedRecommender(datamodel, userneighborhood, usersimilarity); 
-System.out.println("\n\nRecommender");
-System.out.println(recommender);	
+	UserBasedRecommender recommender = new 	GenericUserBasedRecommender(datamodel, userneighborhood, usersimilarity); 	
 	List<RecommendedItem> recommendations = recommender.recommend(2, 3); 
-System.out.println("\n\nRecommendations");
-System.out.println(recommendations);
-	System.out.println("\n\nCreated recommendations");
+	System.out.println("\nCreated recommendations");
 	for (RecommendedItem recommendation : recommendations) { 
 	       System.out.println(recommendation); 
 	}
          }catch(Exception e) { 
 	  System.out.println(e); 
 	  } 
-	System.out.println("\n\ncompleted execution"); 
+	System.out.println("\n");
+
+
      } 
 } 
